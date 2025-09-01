b91eae6641427a8b49f81372b3b2905ac968091e
Thanh2901 <thanhvuworkspace@gmail.com>
2025-09-01 14:47:39 +0700

    update README.md

--- .history/employees_management/employees/urls_20250901093259.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("employees/", views.EmployeeListCreateView, name="employees-list-create"),
-    path("work-schedules/upsert/", views.WorkScheduleUpdateOrInsertView, name="work-schedule-upsert"),
-]
--- .history/employees_management/employees/views_20250831204855.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import generics, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.db.models import Q
-from .models import Employee
-from .serializers import EmployeeSerializer, WorkScheduleUpsertSerializer
